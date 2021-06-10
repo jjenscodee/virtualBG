f84913520d775080b51129ef344e73e57b211e75
Jenscode-trash <71389754+jjenscodee@users.noreply.github.com>
2021-06-10 07:40:39 +0200

    finish implementation

--- virtualBG/DepthFilter.swift
+++ virtualBG/DepthFilter.swift
@@ -7,29 +7,57 @@
 
 import Foundation
 import CoreImage
-
+import UIKit
+import AVFoundation
+import Accelerate
 
 class DepthFilters {
-  func createMask(for depthImage: CIImage,
-                          isSharp: Bool = false) -> CIImage {
-    let focus = 0.99
-    let scale = 3.75
-    let filterWidth =  2 / 4 + 0.1
-    let b = -4 * (CGFloat(focus) - CGFloat(filterWidth) / 2)
-
+    func createMask(for depthImage: CIImage, slope: CGFloat, adjust: CGFloat, flag: Bool) -> CIImage {
+        
+    let focus = 0.99 //0.99
+    // the larger scale, the steeper slope
+        let scale = 3.75
+    
+    var automatic_adjust = adjust
+    var filterwidth =  0.1 + CGFloat(2 / scale)
+    
+    // moves the line left or right //plus to move right
+    var b = -CGFloat(scale) * (CGFloat(focus) - filterwidth / CGFloat(2))
+        
+    if(flag == false){
+        filterwidth =  0.1 + CGFloat(2 / slope)
+        //b = -CGFloat(slope) * (CGFloat(focus) - filterwidth / CGFloat(2))
+        b = slope
+        //print(b)
+    }
+        
+    if(flag == true){
+        if(automatic_adjust != -1){
+            //b = -CGFloat(scale) * (CGFloat(focus) - filterwidth / CGFloat(2)) - (1/automatic_adjust*1000)
+            b = -automatic_adjust
+            
+        }
+    }
+    //b = 0
+    
+    
+    
     let mask = depthImage
       .applyingFilter("CIColorMatrix", parameters: [
-        "inputRVector": CIVector(x: 4, y: 0, z: 0, w: 0),
-        "inputGVector": CIVector(x: 0, y: 4, z: 0, w: 0),
-        "inputBVector": CIVector(x: 0, y: 0, z: 4, w: 0),
-        "inputBiasVector": CIVector(x: b-1, y: b-1, z: b-1, w: 0)
+        "inputRVector": CIVector(x: 255, y: 0, z: 0, w: 0),
+        "inputGVector": CIVector(x: 0, y: 255, z: 0, w: 0),
+        "inputBVector": CIVector(x: 0, y: 0, z: 255, w: 0),
+        "inputBiasVector": CIVector(x: b, y: b, z: b, w: 0)
       ])
-      .applyingFilter("CIColorClamp")   //set value between 0 to 1
-      .applyingFilter("CIBicubicScaleTransform", parameters: [
-        "inputScale": scale
+        .applyingFilter("CIColorClamp")   //set value between 0 to 1
+      .applyingFilter("CILanczosScaleTransform", parameters: [
+        "inputScale": 3.75
       ]) //bicubic scale transform filter
-
+    
+   // print(mask)
     return mask
+        
+        
   }
 
   
--- virtualBG/DepthStream.swift
+++ virtualBG/DepthStream.swift
@@ -8,10 +8,25 @@
 import Foundation
 import UIKit
 import AVFoundation
+import Accelerate
 
 class DepthVideoViewController: UIViewController {
   @IBOutlet weak var previewView: UIImageView!
-  
+  @IBOutlet weak var sliderVal: UISlider!
+  var sliderValue: CGFloat = 0.0
+  @IBAction func slider(_ sender: UISlider) {
+        sliderValue = CGFloat(sliderVal.value)
+    }
+  var automatic_flag = false
+  @IBOutlet var mySwitch: UISwitch!
+    @IBAction func switchDidChange(_ sender: UISwitch){
+        if sender.isOn{
+            automatic_flag = true
+        }
+        else{
+            automatic_flag = false
+        }
+    }
   let session = AVCaptureSession()
   let depthOutput = AVCaptureDepthDataOutput()
   let videoOutput = AVCaptureVideoDataOutput()
@@ -24,11 +39,12 @@ class DepthVideoViewController: UIViewController {
   
   var depthMap: CIImage?
   var depthFilters = DepthFilters()
-
+  var update_adjust: Timer?
+  var automatic_adjust = CGFloat(0)
     
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    update_adjust = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
     configureCaptureSession()
 
     session.startRunning()
@@ -36,6 +52,10 @@ class DepthVideoViewController: UIViewController {
 }
 
 extension DepthVideoViewController {
+    @objc func runTimedCode() {
+        automatic_adjust = getHistogram(depthMap!)
+    }
+    
   func configureCaptureSession() {
     guard let camera = AVCaptureDevice.default(.builtInTrueDepthCamera, for: .video, position: .front) else {
       fatalError("No depth video camera available")
@@ -72,7 +92,7 @@ extension DepthVideoViewController {
     // Search for highest resolution with half-point depth values
     let depthFormats = camera.activeFormat.supportedDepthDataFormats
     let filtered = depthFormats.filter({
-        CMFormatDescriptionGetMediaSubType($0.formatDescription) == kCVPixelFormatType_DepthFloat16
+        CMFormatDescriptionGetMediaSubType($0.formatDescription) == kCVPixelFormatType_DisparityFloat16
     })
     let selectedFormat = filtered.max(by: {
         first, second in CMVideoFormatDescriptionGetDimensions(first.formatDescription).width < CMVideoFormatDescriptionGetDimensions(second.formatDescription).width
@@ -97,6 +117,99 @@ extension DepthVideoViewController {
 }
 
 extension DepthVideoViewController: AVCaptureDataOutputSynchronizerDelegate{
+    func convertCIImageToCGImage(inputImage: CIImage) -> CGImage? {
+        let context = CIContext(options: nil)
+        if let cgImage = context.createCGImage(inputImage, from: inputImage.extent) {
+            return cgImage
+        }
+        return nil
+    }
+    
+
+    func getHistogram(_ image: CIImage)->  CGFloat  {
+        //[Int: Int]
+            guard
+                let cgImage = convertCIImageToCGImage(inputImage: image),
+                var imageBuffer = try? vImage_Buffer(cgImage: cgImage)
+            else {
+                //print("nonononon")
+                return -1
+            }
+            defer {
+                imageBuffer.free()
+            }
+            //print("hihihihihihihihih")
+            var redArray: [vImagePixelCount] = Array(repeating: 0, count: 256)
+            var greenArray: [vImagePixelCount] = Array(repeating: 0, count: 256)
+            var blueArray: [vImagePixelCount] = Array(repeating: 0, count: 256)
+            var alphaArray: [vImagePixelCount] = Array(repeating: 0, count: 256)
+            var error: vImage_Error = kvImageNoError
+
+            redArray.withUnsafeMutableBufferPointer { rPointer in
+                greenArray.withUnsafeMutableBufferPointer { gPointer in
+                    blueArray.withUnsafeMutableBufferPointer { bPointer in
+                        alphaArray.withUnsafeMutableBufferPointer { aPointer in
+
+                var histogram = [ rPointer.baseAddress, gPointer.baseAddress, bPointer.baseAddress, aPointer.baseAddress ]
+                histogram.withUnsafeMutableBufferPointer { hPointer in
+                  
+                  if let hBaseAddress = hPointer.baseAddress {
+                    
+                    error = vImageHistogramCalculation_ARGB8888(&imageBuffer, hBaseAddress, UInt32(kvImageNoFlags))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+          
+        var max1 = -1
+        var maxval1 = -1
+        var max2 = -1
+        var maxval2 = -1
+        
+        
+        for i in 1..<255{
+            //print("hi")
+            if (redArray[i-1] < redArray[i] && redArray[i] > redArray[i+1]){
+                if(maxval1 >= maxval2){
+                    if(redArray[i] > maxval2){
+                        max2 = i
+                        maxval2 = Int(Double(redArray[i]))
+                    }
+                }
+                else{
+                    if(redArray[i] > maxval1){
+                        max1 = i
+                        maxval1 = Int(Double(redArray[i]))
+                    }
+                }
+            }
+        }
+        
+        if(redArray[255] > redArray[254]){
+            
+            if(maxval1 >= maxval2){
+                if(redArray[255] > maxval2){
+                    max2 = 255
+                    maxval2 = Int(Double(redArray[255]))
+                }
+            }
+            else{
+                if(redArray[255] > maxval1){
+                    max1 = 255
+                    maxval1 = Int(Double(redArray[255]))
+                }
+            }
+        }
+        
+        
+        
+        return CGFloat(abs(max1 + max2))/2
+        
+        }
+
+    
     func dataOutputSynchronizer(_ synchronizer: AVCaptureDataOutputSynchronizer, didOutput synchronizedDataCollection: AVCaptureSynchronizedDataCollection) {
         
         guard let syncedDepthData: AVCaptureSynchronizedDepthData = synchronizedDataCollection.synchronizedData(for: depthOutput) as? AVCaptureSynchronizedDepthData,
@@ -113,17 +226,18 @@ extension DepthVideoViewController: AVCaptureDataOutputSynchronizerDelegate{
         // depth map
         let depthData = syncedDepthData.depthData
         var convertedDepth: AVDepthData
-        let depthDataType = kCVPixelFormatType_DisparityFloat32
+        // get disparity
+        let depthDataType = kCVPixelFormatType_DepthFloat32
         if depthData.depthDataType != depthDataType {
           convertedDepth = depthData.converting(toDepthDataType: depthDataType)
         } else {
           convertedDepth = depthData
         }
         let pixelBuffer = convertedDepth.depthDataMap
-        let depthMap = CIImage(cvPixelBuffer: pixelBuffer)
+        depthMap = CIImage(cvPixelBuffer: pixelBuffer)
         //print(depthMap)
         //print()
-        
+
         // video to image
         let sampleBuffer = syncedVideoData.sampleBuffer
         guard let videoPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
@@ -134,16 +248,23 @@ extension DepthVideoViewController: AVCaptureDataOutputSynchronizerDelegate{
         // get background
         let background: CIImage! = CIImage(image: BG.background!)
         
+        // automanic
+        print( getHistogram(depthMap!))
+        //automatic_adjust = getHistogram(depthMap!)
         // create mask
-        let masks = depthFilters.createMask(for: depthMap,
-                                               isSharp: false)
+        //print(automatic_flag)
+        let masks = depthFilters.createMask(for: depthMap!, slope: sliderValue, adjust: automatic_adjust, flag: automatic_flag)
         
         // get filtered image
         let previewImage: CIImage
         previewImage = depthFilters.virtualBG(image: image,
                                                 background: background,
                                                 mask: masks)
+        
         let displayImage = UIImage(ciImage: previewImage)
+        //let displayImage = UIImage(ciImage: masks)
+        //let displayImage = UIImage(ciImage: depthMap!)
+        //let displayImage = UIImage(ciImage: image)
         
         DispatchQueue.main.async { [weak self] in
             
